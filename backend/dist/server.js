abba25281f57d9f5858822a0a613428c7be6c402
Mateus Machado <mateusvmachado@outlook.com>
2018-12-20 13:37:04 -0200

    testeeee

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -3,16 +3,16 @@ exports.__esModule = true;
 var jsonServer = require("json-server");
 var fs = require("fs");
 var https = require("https");
-var auth_1 = require("./auth");
-var authz_1 = require("./authz");
 var server = jsonServer.create();
 var router = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
-server.post('/login', auth_1.handleAuthentication);
-server.use('/orders', authz_1.handleAuthorization);
-server.use(router);
+server.post('/login', function (req, resp) {
+    resp.json({ message: 'okk' });
+});
+//server.use('/orders', handleAuthorization)
+//server.use(router)
 var options = {
     cert: fs.readFileSync('./backend/keys/cert.pem'),
     key: fs.readFileSync('./backend/keys/key.pem')
